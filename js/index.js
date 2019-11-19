550b825dfa3b7aa857a0563d6902ac801b635b7d
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-11-19 19:59:43 +0000

    Display four suits of cards

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-const suit = 'hearts';
+const suit = ['hearts', 'spades', 'diamonds', 'clubs'];
 const cardsWrapper = document.querySelector('.cards-wrapper');
 
 function createCards() {
   const cards = [];
   // Create an array with objects containing the value and the suit of each card
-  for (let i = 1; i <= 13; i += 1) {
+  for (let i = 0; i < 52; i += 1) {
     const cardObject = {
-      value: i,
-      suit,
+      value: i % 13,
+      suit: suit[Math.floor(i / 13)],
     };
     cards.push(cardObject);
   }
 
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
-    const positionFromLeft = i * 15;
+    const positionFromLeft = i * 22;
     const cardElement = document.createElement('div');
     cardElement.setAttribute('data-value', card.value);
     cardElement.classList.add('card', `${card.suit}-${card.value}`);
