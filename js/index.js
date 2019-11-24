e70ccc456a781c1178313b17b14dc890b5b3a3c2
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-11-24 19:14:50 +0000

    Add toggling show and hide function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const suit = ['hearts', 'spades', 'diamonds', 'clubs'];
 const cardsWrapper = document.querySelector('.cards-wrapper');
-const cardSpacing = 22;
+const cardSpacing = 20;
 
 function createCards() {
   const cards = [];
@@ -15,8 +15,9 @@ function createCards() {
 
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
-    const positionFromLeft = i * 22;
+    const positionFromLeft = i * cardSpacing;
     const cardElement = document.createElement('div');
+    cardElement.setAttribute('suit', card.suit);
     cardElement.setAttribute('data-value', card.value);
     cardElement.setAttribute('sorted-pos', i);
     cardElement.classList.add('card', `${card.suit}-${card.value}`);
@@ -32,6 +33,13 @@ function createButtons() {
   const gameButtons = document.getElementById('buttons');
   startButton.style.display = "none";
   gameButtons.style.display = "block";
+}
+
+// Function to start the game by clearing the wrapper, creating
+// and appending the buttons and all the cards to the DOM
+function startGame() {
+  createButtons();
+  createCards();
 }
 
 function shuffleCards() {
@@ -59,12 +67,27 @@ function fixPadding(index) {
   cardsWrapper.childNodes[index].style.left = `${(index - 1) * cardSpacing}px`;
 }
 
-// Function to start the game by clearing the wrapper, creating
-// and appending the buttons and all the cards to the DOM
-function startGame() {
-  createButtons();
-  createCards();
+function toggleHideAndShow() {
+  let length = cardsWrapper.childElementCount;
+  for (let i = 1; i <= length; i++) {
+    let card = cardsWrapper.childNodes[i];
+    if (card.classList.contains('hidden')) {
+      card.className = "";
+      const suit = card.getAttribute('suit');
+      const value = card.getAttribute('data-value');
+      card.classList.add('card', `${suit}-${value}`);
+    } else {
+      card.className = "";
+      card.classList.add('hidden', 'card');
+    }
+  }
+}
+
+function sortCards() {
+
 }
 
 document.getElementById('start-game').addEventListener('click', startGame);
 document.getElementById('shuffle').addEventListener('click', shuffleCards);
+document.getElementById('show-hide').addEventListener('click', toggleHideAndShow)
+document.getElementById('magic').addEventListener('click', sortCards);
