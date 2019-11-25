627df128782f9f78f12b5fd34a3e3478d24529d4
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-11-25 12:17:11 +0000

    Uncomment all tests

--- cypress/integration/magic-trick.js
+++ cypress/integration/magic-trick.js
@@ -16,14 +16,14 @@ describe('Play game', () => {
     cy.get('[class*="diamonds-"]').should('have.length', 13);
     cy.get('[class*="clubs-"]').should('have.length', 13);
 
-    // cy.get('#start-game').should('have.not.exist');
-    // suits.forEach((suit) => {
-    //   cy.get(`[class*="${suit}-"]`).should('have.length', 13);
-    // });
-    // cy.get('.card').then((cards) => {
-    //   const allCardClasses = [...cards].map((card) => card.classList[1]);
-    //   expect(allCardClasses).to.deep.equal(sortedCardsClasses);
-    // });
+    cy.get('#start-game').should('have.not.exist');
+    suits.forEach((suit) => {
+      cy.get(`[class*="${suit}-"]`).should('have.length', 13);
+    });
+    cy.get('.card').then((cards) => {
+      const allCardClasses = [...cards].map((card) => card.classList[1]);
+      expect(allCardClasses).to.deep.equal(sortedCardsClasses);
+    });
 
     cy.contains('Shuffle').click();
     cy.get('.card').then((cards) => {
@@ -31,11 +31,11 @@ describe('Play game', () => {
       expect(allCardClasses).to.not.deep.equal(sortedCardsClasses);
     });
 
-    // cy.contains('Show/Hide').click();
-    // cy.get('.cards-wrapper').should('have.class', 'hidden');
+    cy.contains('Show/Hide').click();
+    cy.get('.cards-wrapper').should('have.class', 'hidden');
 
-    // cy.contains('Show/Hide').click();
-    // cy.get('.cards-wrapper').should('not.have.class', 'hidden');
+    cy.contains('Show/Hide').click();
+    cy.get('.cards-wrapper').should('not.have.class', 'hidden');
 
     cy.contains('Magic').click();
     cy.get('.card').then((cards) => {
